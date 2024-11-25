3fdb1aeb16f2ccd8c93c323217325c39e30588db
Mari Lehtikari <mari.lehtikari@helsinki.fi>
2024-11-25 12:00:23 +0200

    korjattu lomakkeen urlit

--- src/app.py
+++ src/app.py
@@ -35,39 +35,43 @@ def handle_type():
             raise ValueError("Valitse viitetyyppi!")
 
         tyyppi = check_citation_type(content)
-        if tyyppi == "book":
-            return render_template("book.html")
-        if tyyppi == "article":
-            return render_template("article.html")
-        if tyyppi == "inproceedings":
-            return render_template("inproceedings.html")
+        if tyyppi in ["book", "article", "inproceedings"]:
+            return redirect(url_for("render_specific_type", tyyppi=tyyppi))
 
     except Exception as error:
         flash(str(error))
         return redirect_to_uusi_viite()
-
+    
+@app.route("/luo-viite/<tyyppi>")
+def render_specific_type(tyyppi):
+    if tyyppi == "book":
+        return render_template("book.html")
+    if tyyppi == "article":
+        return render_template("article.html")
+    if tyyppi == "inproceedings":
+        return render_template("inproceedings.html")
 
 @app.route("/luo-viite2", methods=["POST"])
 def cite_creation2():
-    type = request.form.get("type")
-    if type == "book":
+    tyyppi = request.form.get("type")
+    if tyyppi == "book":
         author = request.form.get("author")
         publisher = request.form.get("publisher")
         year = request.form.get("year")
         title = request.form.get("title")
-        create_citation(type, author, publisher, year, title)
-    if type == "article":
+        create_citation(tyyppi, author, publisher, year, title)
+    if tyyppi == "article":
         author = request.form.get("author")
         journal = request.form.get("journal")
         year = request.form.get("year")
         title = request.form.get("title")
-        create_citation(type, author, None, year, title, journal)
-    if type == "inproceedings":
+        create_citation(tyyppi, author, None, year, title, journal)
+    if tyyppi == "inproceedings":
         author = request.form.get("author")
         booktitle = request.form.get("booktitle")
         year = request.form.get("year")
         title = request.form.get("title")
-        create_citation(type, author, None, year, title, None, booktitle)
+        create_citation(tyyppi, author, None, year, title, None, booktitle)
     return redirect("/")
 
 
