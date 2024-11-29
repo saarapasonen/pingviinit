560dc4d4483390d24571de8ac2b52161ccdd88c8
Filippa Högnäsbacka <filippa.hognasbacka@gmail.com>
2024-11-29 21:36:15 +0200

    update citation function

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 import datetime
 from flask import redirect, render_template, request, jsonify, flash, url_for, Response
 from db_helper import reset_db
-from repositories.todo_repository import get_cites, create_citation, check_citation_type
+from repositories.todo_repository import (
+    get_cites, create_citation, check_citation_type, get_cite_by_id, update_citation
+)
 from config import app, test_env
 
 def redirect_to_uusi_viite():
@@ -26,10 +28,76 @@ def lisatyt():
     cites = get_cites()
     return render_template("lisatyt.html", cites=cites)
 
-@app.route("/muokkaa_viitetta")
-def muokkaa():
-    cites = get_cites()
-    return render_template("muokkaa_viitetta.html", cites=cites)
+@app.route("/muokkaa_viitetta", methods=["GET"])
+@app.route("/muokkaa_viitetta/<int:viite_id>", methods=["GET", "POST"])
+def edit_citation(viite_id=None):
+    if request.method == "GET":
+        if viite_id:
+            cite = get_cite_by_id(viite_id)
+            return render_template("muokkaa_viitetta.html", cite=cite)
+        else:
+            cites = get_cites()
+            return render_template("muokkaa_viitetta.html", cites=cites)
+
+    if request.method == "POST":
+        viite_id = request.form.get("type")
+        author = request.form.get("author")
+        title = request.form.get("title")
+        year = request.form.get("year")
+        publisher = request.form.get("publisher")
+        journal = request.form.get("journal")
+        booktitle = request.form.get("booktitle")
+        vuosi = datetime.date.today().year
+        try:
+            if viite_id == "book":
+                author = request.form.get("author")
+                publisher = request.form.get("publisher")
+                year = request.form.get("year")
+                title = request.form.get("title")
+                if not author or not publisher or not year or not title:
+                    raise ValueError("Täytä kaikki kentät")
+                if len(author) > 500 or len(publisher) > 500 or len(title) > 500:
+                    raise ValueError("Yhteen kenttään voi kirjoittaa max. 500 merkkiä")
+                if int(year) > vuosi or year.isnumeric() is False:
+                    raise ValueError(
+                        "Vuosiluku tulee olla välillä 0-nyt. Vuosiluvun tulee olla numeromuodossa."
+                        )
+
+                update_citation(viite_id, author, publisher, year, title)
+            if viite_id == "article":
+                author = request.form.get("author")
+                journal = request.form.get("journal")
+                year = request.form.get("year")
+                title = request.form.get("title")
+                if not author or not journal or not year or not title:
+                    raise ValueError("Täytä kaikki kentät")
+                if len(author) > 500 or len(journal) > 500 or len(title) > 500:
+                    raise ValueError("Yhteen kenttään voi kirjoittaa max. 500 merkkiä")
+                if int(year) > vuosi or year.isnumeric() is False:
+                    raise ValueError(
+                        "Vuosiluku tulee olla välillä 0-nyt. Vuosiluvun tulee olla numeromuodossa."
+                        )
+
+                update_citation(viite_id, author, None, year, title, journal)
+            if viite_id == "inproceedings":
+                author = request.form.get("author")
+                booktitle = request.form.get("booktitle")
+                year = request.form.get("year")
+                title = request.form.get("title")
+                if not author or not booktitle or not year or not title:
+                    raise ValueError("Täytä kaikki kentät")
+                if len(author) > 500 or len(booktitle) > 500 or len(title) > 500:
+                    raise ValueError("Yhteen kenttään voi kirjoittaa max. 500 merkkiä")
+                if int(year) > vuosi or year.isnumeric() is False:
+                    raise ValueError(
+                        "Vuosiluku tulee olla välillä 0-nyt. Vuosiluvun tulee olla numeromuodossa."
+                        )
+                update_citation(viite_id, author, None, year, title, None, booktitle)
+
+            return redirect("/muokkaa_viitetta")
+        except ValueError as error:
+            flash(str(error))
+            return redirect(url_for("edit_citation", viite_id=viite_id))
 
 @app.route("/uusi_viite", methods=["GET"])
 def render_uusi_viite():
